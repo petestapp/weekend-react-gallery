5f79ace81e15dc0f45999458cf39d6e4223d3ceb
petestapp <63821853+petestapp@users.noreply.github.com>
2021-10-31 13:44:34 -0500

    photos displaying and toggling when clicked

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,12 +1,26 @@
+import axios from "axios";
 import {useState} from "react";
 
 function GalleryItem(props){
+    const[display, setDisplay]=useState(true);
+
+    const toggleDisplay=()=>{
+        setDisplay(!display);
+    }
+    const addLike=()=>{
+        console.log(`in addLike`);
+    }
+
     //const[name, setName] = useState(null);
     return(
         <div>
-            <p>{props.image.path}</p>
-            <p>{props.image.description}</p>
+            {
+                display ?
+                <img onClick={toggleDisplay} src={props.image.path} alt={props.image.description} /> :
+                <p onClick={toggleDisplay} >{props.image.description}</p>
+            }
             <p>{props.image.likes}</p>
+            <button onClick={addLike}>like</button>
         </div>
     )
 }
